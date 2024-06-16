3eab82f55364be16519d9458f216d2274705ad49
HashiruG <hashiruhgunathilake@gmail.com>
2024-06-16 22:21:32 +0530

    Handle form submission in addItem component

--- components/addItem/AddItem.js
+++ components/addItem/AddItem.js
@@ -1,18 +1,34 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useState } from 'react';
 
 const AddItem = (props) => {
+
+    const [formData, setformData] = useState({recipeName:"", price:""})
+    function handleChange(name, value){
+            setformData((prevvalue) =>{
+                return({...prevvalue,
+                    [name] : value
+                })
+            })
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        props.onFormSubmit(formData)
+    }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" >
         <Form.Label>{props.firstInput}</Form.Label>
-        <Form.Control type="text"  />
+        <Form.Control name="recipeName" type="text"  onChange={(e) => {handleChange(e.target.name, e.target.value)}}/>
         
       </Form.Group>
 
       <Form.Group className="mb-3" >
         <Form.Label>{props.secondInput}</Form.Label>
-        <Form.Control type="password" />
+        <Form.Control name="price" type="number" onChange={(e) => {handleChange(e.target.name, e.target.value)}}/>
       </Form.Group>
       
       <Button variant="primary" type="submit">
