1ee14cd80bfcabafe5c05a4eaed94848eec5d7bb
HashiruG <hashiruhgunathilake@gmail.com>
2024-08-28 10:38:16 +0530

    Final responsive check

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,6 +1,9 @@
+import React from 'react';
 import { useState } from "react";
+import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import styles from './login.module.css';
 
-const LoginItem = (props) => {
+export default function LoginPage(props) {
     const [formData, setformData] = useState({ email: "", password: "" });
 
     function handleChange(name, value) {
@@ -17,72 +20,62 @@ const LoginItem = (props) => {
         }
     }
 
+
     return (
-        <div className="vh-100 d-flex justify-content-center align-items-center" style={{ backgroundColor: '#343a40' }}>
-            <section className="gradient-form" style={{ padding: '20px' }}>
-                <div className="container py-5">
-                    <div className="row d-flex justify-content-center align-items-center">
-                        <div className="col-xl-10">
-                            <div className="card rounded-3 border border-3 text-black bg-light">
-                                <div className="row g-0">
-                                    <div className="col-lg-6">
-                                        <div className="card-body p-md-5 mx-md-4">
-                                            <div className="text-center">
-                                                <h2 className="fw-bold mb-2 text-uppercase">Welcome to Mixmatic Pro</h2>
-                                                <h4 className="mt-1 mb-5 pb-1">Sign in to your account</h4>
-                                            </div>
-                                            <form onSubmit={handleSubmit}>
-                                                <div className="form-outline mb-4">
-                                                    <label className="form-label" htmlFor="form2Example11">Email</label>
-                                                    <input
-                                                        type="email"
-                                                        name="email"
-                                                        id="form2Example11"
-                                                        className="form-control"
-                                                        placeholder="Email Address"
-                                                        onChange={(e) => handleChange(e.target.name, e.target.value)}
-                                                    />
-                                                </div>
-                                                <div className="form-outline mb-4">
-                                                    <label className="form-label" htmlFor="form2Example22">Password</label>
-                                                    <input
-                                                        type="password"
-                                                        name="password"
-                                                        id="form2Example22"
-                                                        className="form-control"
-                                                        placeholder="Password"
-                                                        onChange={(e) => handleChange(e.target.name, e.target.value)}
-                                                    />
-                                                </div>
-                                                <div className="text-center pt-1 mb-5 pb-1">
-                                                    <button
-                                                        className="btn btn-dark btn-block fa-lg gradient-custom-2 mb-3"
-                                                        type="submit"
-                                                    >
-                                                        Sign in
-                                                    </button>
-                                                    <p style={{ color: "red" }}>{props.message}</p>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 d-flex align-items-center justify-content-center gradient-custom-2"  style={{ backgroundColor: 'white' }}>
-                                        <div className="text-center">
-                                            <img
-                                                src="/image.jpeg"
-                                                style={{ maxWidth: '100%', height: 'auto' }}
-                                                alt="Right Side Image"
-                                            />
-                                        </div>
-                                    </div>
+        <div className={styles.container}>
+            <Container
+                fluid
+                className={styles.wrapper}
+            >
+                <Row className={styles.row}>
+                   
+                    <Col md={6} className={styles.col}>
+                        <Card className={styles.card}>
+                            <Card.Body className={styles.cardContent}>
+                                <div className="text-center mb-4">
+                                    <h2 className="fw-bold text-uppercase">Welcome to Mixmatic Pro</h2>
+                                    <h4 className="mt-1 mb-5">Sign in to your account</h4>
                                 </div>
-                            </div>
+                                <Form onSubmit={handleSubmit}>
+                                    <Form.Group className="mb-4" controlId="formEmail">
+                                        <Form.Label>Email</Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            name="email"
+                                            placeholder="Email Address"
+                                            onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <Form.Group className="mb-4" controlId="formPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            placeholder="Password"
+                                            onChange={(e) => handleChange(e.target.name, e.target.value)}
+                                        />
+                                    </Form.Group>
+                                    <div className="text-center">
+                                        <Button variant="dark" type="submit" className="w-100 mb-3">
+                                            Sign in
+                                        </Button>
+                                        {props.message && <p style={{color:"red"}}>{props.message}</p>}
+                                    </div>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+               
+                    <Col md={6} className={styles.col}>
+                        <div className={styles.imageContainer}>
+                            <img
+                                src="/image.jpeg"
+                                alt="Side Image"
+                            />
                         </div>
-                    </div>
-                </div>
-            </section>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
-
-export default LoginItem;
